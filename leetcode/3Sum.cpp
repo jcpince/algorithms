adfe3ae14d474f4cdebb06c34f86252b7f6ccf6d
Jean-Christophe Pince <jcpince@gmail.com>
2019-06-23 14:41:44 +0200

    Improve 3Sum algo.
    Improve from n*n*log(n) to n*n(rough).
    
    Signed-off-by: Jean-Christophe Pince <jcpince@gmail.com>

--- leetcode/3Sum.cpp
+++ leetcode/3Sum.cpp
@@ -23,60 +23,27 @@
 #include <vector>
 #include <set>
 
+//#define DEBUG 1
+#undef DEBUG
+#define COUNT_ITERATIONS 1
+
 #include <UnitTests.h>
 #include <TestsHelper.h>
 
 using namespace std;
 
-//#define DEBUG 1
-#undef DEBUG
-
-void inline encode(uint64_t &encoded, int first, int second, int third)
-{
-    int upper = min(min(first, second), third);
-    int lower;
-    if (upper == first) lower = min(second, third);
-    else if (upper == second) lower = min(first, third);
-    else lower = min(first, second);
-    encoded = ((uint64_t)upper << 32) | ((uint64_t)lower & 0xffffffff);
-    debug_print("encode(%d, %d, %d) => 0x%lx\n", first, second, third, encoded);
-}
-
-void inline decode(uint64_t encoded, int &first, int &second, int &third)
-{
-    first = (int)(encoded >> 32);
-    second = (int)(encoded & 0xffffffff);
-    third = 0 - first - second;
-    debug_print("decode(0x%lx) => %d, %d, %d\n", encoded, first, second, third);
-}
-
 const bool continue_on_failure = true;
 
 class Solution {
-private:
-    vector<vector<int>> set2vector(set<uint64_t> results)
-    {
-        vector<vector<int>> vresults;
-        vresults.reserve(results.size());
-        for (uint64_t encoded : results)
-        {
-            int first, second, third;
-            decode(encoded, first, second, third);
-            vector<int> result = {first, second, third};
-            vresults.push_back(result);
-        }
-        return vresults;
-    }
-
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
+    /* Solution n*(n-1)/2* log(n) => n^2 * log(n) */
+    vector<vector<int>> threeSum_nsquare_logn(vector<int>& nums) {
         if (nums.size() < 3) return {};
 
-        set<uint64_t> results;
-        uint64_t encoded;
+        vector<vector<int>> results;
         sort(nums.begin(), nums.end());
 
-        //printf("Sorted array is %s\n", array2str(nums).c_str());
+        debug_print("Sorted array is %s\n", array2str(nums).c_str());
 
         int smallest = nums.front(), biggest = nums.back();
         int end_idx = nums.size() - 1;
@@ -90,11 +57,10 @@ public:
             if (first > 0) break;
             first0 = first;
 
-            int found0 = end_idx+1;
-            //first_found0 = end_idx + 1;
-            //printf("Second loop with idx1 %d\n", idx1);
-            for (int idx2 = idx1 + 1 ; idx2 < end_idx ; idx2++)
+            int found0 = end_idx+1, found0_below = -1;
+            for (int idx2 = idx1 + 1 ; (idx2 < end_idx) && (found0 > 0) ; idx2++)
             {
+                update_iterations();
                 int second = vectorat(nums, idx2);
                 if (second == second0) continue;
                 if ((first + second) > 0) break;
@@ -103,27 +69,82 @@ public:
                 int third = 0 - first - second;
                 if (third < smallest || third > biggest) continue;
 
-                encode(encoded, first, second, third);
+                debug_print("Candidate (%d, %d, %d) - search %d between %d and %d\n",
+                    first, second, third, third, idx2+1, found0-1);
+                int found = find_element_or_below(nums, third, idx2+1, found0-1, found0_below);
 
-                if (results.find(encoded) != results.end()) continue;
-
-                /*printf("Candidate (%d, %d, %d) - search %d between %d and %d\n",
-                    first, second, third, third, idx2+1, found0-1);*/
-                int found = find_element(nums, third, idx2+1, found0-1);
-
-                //printf("Here third is %d, idx1(%d), idx2(%d), found0(%d) => %d\n", third, idx1, idx2, found0, found);
+                debug_print("Here third is %d, idx1(%d), idx2(%d), found0(%d) => %d\n", third, idx1,
+                 idx2, found0, found);
                 if (found != -1)
                 {
-                    //printf("Found is %d, Insert %d, %d, %d\n", found, first, second, third);
-                    // printf("Found is %d, Insert %d, %d, %d\n", found, first, second, third);
-                    assert(found < found0);
-                    results.insert(encoded);
+                    debug_print("Found is %d, Insert %d, %d, %d\n", found, first, second, third);
+                    dev_assert(found < found0);
+                    results.push_back({first, second, third});
                     found0 = found;
-                    //if (first_found0 == (end_idx+1)) first_found0 = found0;
+                }
+                else if (found0_below != -1) found0 = found0_below + 1;
+            }
+        }
+        print_iterations_count();
+        return results;
+    }
+
+    /* Solution n*n (discounting the duplicates skipping) */
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int elems_count = nums.size();
+        if (elems_count < 3) return {};
+
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> result;
+        reset_iteration();
+
+        for (int pivot_idx = 0 ; pivot_idx < elems_count - 2 ; pivot_idx++)
+        {
+            /* Skip the next pivot if identical to the previous */
+            int pivot_value = vectorat(nums, pivot_idx);
+            if ((pivot_idx > 0) and
+                (vectorat(nums, pivot_idx - 1) == pivot_value)) continue;
+
+            /* if the pivot is > 0, there's no sum == 0 remaining */
+            if (pivot_value > 0) break;
+
+            int small_idx = pivot_idx + 1, large_idx = elems_count - 1;
+            while (small_idx < large_idx)
+            {
+                update_iterations();
+                int small_value = vectorat(nums, small_idx);
+
+                /* if n1 + n2 > 0, n2 is > 0 and n3 will be as well => break */
+                int sum = pivot_value + small_value;
+                if (sum > 0) break;
+
+                int large_value = vectorat(nums, large_idx);
+                sum += large_value;
+
+                if (sum > 0) large_idx--;
+                else if (sum < 0) small_idx++;
+                else {
+                    /* Found a solution, store it */
+                    result.push_back({pivot_value, small_value, large_value});
+
+                    /* move forward and skip duplicates */
+                    do {
+                        update_iterations();
+                        small_idx++;
+                        if (small_idx >= large_idx) break;
+                    } while (vectorat(nums, small_idx) == vectorat(nums, small_idx - 1));
+
+                    do
+                    {
+                        update_iterations();
+                        large_idx--;
+                        if (small_idx >= large_idx) break;
+                    } while (vectorat(nums, large_idx) == vectorat(nums, large_idx + 1));
                 }
             }
         }
-        return set2vector(results);
+        print_iterations_count();
+        return result;
     }
 };
 
@@ -133,6 +154,7 @@ int run_test_case(void *_s, TestCase *tc)
     vector<vector<int>> expected = tc->test_case[JSON_TEST_CASE_EXPECTED_FIELDNAME];
 
     vector<vector<int>> result = ((Solution *)_s)->threeSum(in);
+    debug_print("Found %ld solutions\n", result.size());
     if (check_result(result, expected)) return 0;
 
     string sin = array2str(in), sout = array2str(result), sexp = array2str(expected);
--- tools/TestsHelper.h
+++ tools/TestsHelper.h
@@ -13,11 +13,23 @@ using namespace std;
 #define vectorat(v, idx) v[idx]
 #define debug_print printf
 #define dev_assert assert
-
 #else
 #define vectorat(v, idx) v.at(idx)
 #define debug_print(...)
 #define dev_assert(...)
+#endif
+
+#ifdef COUNT_ITERATIONS
+uint64_t iterations_count = 0;
+#define reset_iteration() do {iterations_count = 0;} while(0)
+#define update_iterations() do {iterations_count++;} while(0)
+#define get_iterations_count() iterations_count
+#define print_iterations_count() printf("%ld iterations\n", iterations_count);
+#else
+#define reset_iteration()
+#define update_iterations()
+#define get_iterations_count()
+#define print_iterations_count()
 #endif
 
 template <class T>
@@ -45,7 +57,7 @@ bool check_result(vector<vector<T>> &result, vector<vector<T>> &expected)
             size_t idx = 0;
             for ( ; idx < e.size() ; idx++)
             {
-                if (e.at(idx) != v.at(idx)) break;
+                if (vectorat(e, idx) != vectorat(v, idx)) break;
             }
             if (idx == e.size()) { found = true; break; }
         }
@@ -76,24 +88,139 @@ string array2str(vector<vector<T>> &varr)
 
 /* binary search / dichotomy */
 template <class T>
-int find_element(vector<T> array, T value, int start, int end)
+int find_element(vector<T> array, T value, int start, int end, int &below_idx, int &above_idx)
 {
 #ifdef DEBUG
     string sin = array2str(array);
-    debug_print("find_third(%s, %d, %d, %d) ", sin.c_str(), value, start, end);
+    debug_print("find_element(%s, %d, %d, %d) ", sin.c_str(), value, start, end);
 #endif
 
-    T middle = start + (end - start + 1) / 2;
+    if (value > vectorat(array, end)) {
+        below_idx = end;
+        above_idx = -1;
+        return -1;
+    }
+    if (value < vectorat(array, start)) {
+        below_idx = -1;
+        above_idx = start;
+        return -1;
+    }
+
+    int middle = start + (end - start + 1) / 2;
     while ((middle <= end) && (middle >= start))
     {
+        update_iterations();
+        T m = vectorat(array, middle);
+        debug_print("v(%d), m(%d), sme(%d, %d, %d)\n", value, m, start, middle, end);
+        if (m == value) {
+            below_idx = middle, above_idx = middle;
+            while (below_idx > start && (vectorat(array, --below_idx) > value)) update_iterations();
+            while (above_idx < end && (vectorat(array, ++above_idx) < value)) update_iterations();
+            if ((below_idx == start) and (vectorat(array, below_idx) >= value)) below_idx = -1;
+            if ((above_idx == end) and (vectorat(array, above_idx) <= value)) above_idx = -1;
+            debug_print(" ====> returns %d, %d, %d\n", middle, below_idx, above_idx);
+            dev_assert(((below_idx != -1) and (vectorat(array, below_idx) < value)) or
+                ((below_idx == -1) and (vectorat(array, start) >= value)));
+            dev_assert(((above_idx != -1) and (vectorat(array, above_idx) > value)) or
+                ((above_idx == -1) and (vectorat(array, end) <= value)));
+            return middle;
+        }
+        if (value < m)
+        {
+            above_idx = middle;
+            if (end == middle) end = middle-1;
+            else end = middle;
+        }
+        else
+        {
+            below_idx = middle;
+            if (start == middle) start = middle+1;
+            else start = middle;
+        }
+        middle = start + (end - start + 1) / 2;
+    }
+    if ((below_idx == start) and (vectorat(array, below_idx) >= value)) below_idx = -1;
+    if ((above_idx == end) and (vectorat(array, above_idx) <= value)) above_idx = -1;
+    dev_assert(((below_idx != -1) and (vectorat(array, below_idx) < value)) or
+        ((below_idx == -1) and (vectorat(array, start) >= value)));
+    dev_assert(((above_idx != -1) and (vectorat(array, above_idx) > value)) or
+        ((above_idx == -1) and (vectorat(array, end) <= value)));
+    debug_print(" ===> returns -1, %d, %d\n", below_idx, above_idx);
+    return -1;
+}
+
+template <class T>
+int find_element_or_below(vector<T> array, T value, int start, int end, int &below_idx)
+{
+#ifdef DEBUG
+    string sin = array2str(array);
+    debug_print("find_element_or_below(%s, %d, %d, %d) ", sin.c_str(), value, start, end);
+#endif
+
+    if (value > vectorat(array, end)) {
+        below_idx = end;
+        return -1;
+    }
+    if (value < vectorat(array, start)) {
+        below_idx = -1;
+        return -1;
+    }
+
+    int middle = start + (end - start + 1) / 2;
+    while ((middle <= end) && (middle >= start))
+    {
+        update_iterations();
         T m = vectorat(array, middle);
         debug_print("v(%d), m(%d), sme(%d, %d, %d)\n", value, m, start, middle, end);
         if (m == value) {
             debug_print(" ====> returns %d\n", middle);
             return middle;
         }
-        // if ((middle == start) && (value < m)) return -1;
-        // if ((middle == end) && (value > m)) return -1;
+        if (value < m)
+        {
+            if (end == middle) end = middle-1;
+            else end = middle;
+        }
+        else
+        {
+            below_idx = middle;
+            if (start == middle) start = middle+1;
+            else start = middle;
+        }
+        middle = start + (end - start + 1) / 2;
+    }
+    if ((below_idx == start) and (vectorat(array, below_idx) >= value)) below_idx = -1;
+    dev_assert(((below_idx != -1) and (vectorat(array, below_idx) < value)) or
+        ((below_idx == -1) and (vectorat(array, start) >= value)));
+    debug_print(" ===> returns -1, %d\n", below_idx);
+    return -1;
+}
+
+template <class T>
+int find_element(vector<T> array, T value, int start, int end)
+{
+#ifdef DEBUG
+    string sin = array2str(array);
+    debug_print("find_element(%s, %d, %d, %d) ", sin.c_str(), value, start, end);
+#endif
+
+    if (value > vectorat(array, end)) {
+        return -1;
+    }
+    if (value < vectorat(array, start)) {
+        return -1;
+    }
+
+    int middle = start + (end - start + 1) / 2;
+    while ((middle <= end) && (middle >= start))
+    {
+        update_iterations();
+        T m = vectorat(array, middle);
+        debug_print("v(%d), m(%d), sme(%d, %d, %d)\n", value, m, start, middle, end);
+        if (m == value) {
+            debug_print(" ====> returns %d, %d, %d\n", middle);
+            return middle;
+        }
         if (value < m)
         {
             if (end == middle) end = middle-1;
@@ -106,7 +233,7 @@ int find_element(vector<T> array, T value, int start, int end)
         }
         middle = start + (end - start + 1) / 2;
     }
-    debug_print(" ===> returns -1\n");
+    debug_print(" ===> returns -1, %d, %d\n");
     return -1;
 }
 
