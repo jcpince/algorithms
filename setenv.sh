a41a4677e8f7cc8c2a5d1d1ff6ecc80dc4a05fc5
Pince, Jean-christophe <jean-christophe.pince@intel.com>
2019-07-19 10:08:11 +0200

    Cpp templates generation script.
    Signed-off-by: Pince, Jean-christophe <jean-christophe.pince@intel.com>

--- setenv.sh
+++ setenv.sh
@@ -81,4 +81,108 @@ algo_buildall()
     popd >/dev/null
 }
 
+algo_newecpp()
+{
+    NEWCPP=$1
+    if [[ -f ${NEWCPP} ]] ; then
+        echo "File ${NEWCPP} already exists!"
+        return -1
+    fi
+    if [[ ${NEWCPP: -4} != ".cpp" ]]; then
+        echo "Usage algo_newecpp <path to new file.cpp>"
+        return -1
+    fi
+    BASEDIR=$(dirname "${NEWCPP}")
+    BASEFILE=$(basename "${NEWCPP}")
+
+    CMAKEFILE=${BASEDIR}/CMakeLists.txt
+    BASEFILE="${BASEFILE%????}"
+    NEWJSON="${BASEDIR}/${BASEFILE}.unittests.json"
+
+    cat > ${NEWCPP} << EOF
+#include <bits/stdc++.h>
+
+//#define DEBUG 1
+#undef DEBUG
+
+#include <TreeNode.h>
+#include <UnitTests.h>
+#include <TestsHelper.h>
+
+using namespace std;
+
+const bool continue_on_failure = false;
+
+int run_test_case(void *_s, TestCase *tc) {
+    UNUSED(_s);
+    vector<int> vtree = tc->test_case[JSON_TEST_CASE_IN_FIELDNAME]["tree"];
+    int k = tc->test_case[JSON_TEST_CASE_IN_FIELDNAME]["k"];
+    bool expected = tc->test_case[JSON_TEST_CASE_EXPECTED_FIELDNAME];
+
+    struct TreeNode tree[vtree.size()];
+    bzero(tree, vtree.size()*sizeof(struct TreeNode));
+    vector2tree(vtree, tree);
+
+    Solution s;
+    bool result = s.findTarget(tree, k);
+
+    if (result == expected) return 0;
+
+    printf("findTarget(%s, %d) returned %d but expected %d\n", array2str(vtree).c_str(), k,
+        result, expected);
+    assert(continue_on_failure);
+    return 1;
+}
+
+int main(int argc, char **argv) {
+    int tests_ran = 0;
+    const char *tc_id = NULL;
+
+    if (argc < 2 || argc > 3)
+    {
+        cerr << "Usage: " << argv[0] << " <json test file> [test case index or name]" << endl;
+        cerr << "Where: [test case index or name] specifies which test to run (all by default)" <<
+            endl;
+        return -1;
+    }
+
+    UnitTests uts(NULL, &run_test_case, argv[1]);
+
+    if (argc == 3) tc_id = argv[2];
+
+    int errors_count = uts.run_test_cases(tc_id, tests_ran);
+    if (errors_count == 0)
+        cout << "All " << tests_ran << " test(s) succeeded!!!" << endl;
+    else
+        cout << errors_count << " test(s) failed over a total of " << tests_ran << endl;
+
+    return errors_count;
+}
+EOF
+
+    echo >> ${CMAKEFILE}
+    echo "add_executable(${BASEFILE} ${BASEFILE}.cpp)" >> ${CMAKEFILE}
+    echo "target_link_libraries(${BASEFILE} unittests)" >> ${CMAKEFILE}
+
+    if [[ -f ${NEWJSON} ]] ; then
+        echo "File ${NEWJSON} already exists!"
+        return 0
+    fi
+    cat > ${NEWJSON} << EOF
+{
+    "tests" :
+    [
+        {
+            "in"        :
+            {
+                "tree"  : [5,3,6,2,4,-1,7],
+                "k"     : 9
+            },
+            "expected"  : true
+        }
+    ]
+}
+EOF
+}
+
 export PATH=${ALGO_BUILDDIR}/bin:$PATH
